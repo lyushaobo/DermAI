ae2eff04f4bc9ea2178bba243d664bd112aa1336
shaobo <shaobolyu2001@gmail.com>
2024-03-25 20:07:15 +0800

    init

---
+++ src/main/java/com/dermai/DermAiApplication.java
@@ -0,0 +1,13 @@
+package com.dermai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DermAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dermai/DermAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
