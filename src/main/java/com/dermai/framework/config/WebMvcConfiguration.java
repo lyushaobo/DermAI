7d2da709057ca7abd48d3f5274545f26316a8b37
shaobo <shaobolyu2001@gmail.com>
2024-03-26 21:49:50 +0800

    add @Configuration

--- src/main/java/com/dermai/framework/config/WebMvcConfiguration.java
+++ src/main/java/com/dermai/framework/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.dermai.framework.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,6 +11,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     /**
      * Generate Interface Documentation via knife4j
