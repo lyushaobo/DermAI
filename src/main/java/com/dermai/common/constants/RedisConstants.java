5899da210f8b5ba9bd54f99715b356bc88b0f9f0
shaobo <shaobolyu2001@gmail.com>
2024-03-29 23:38:15 +0800

    add redis constants

---
+++ src/main/java/com/dermai/common/constants/RedisConstants.java
@@ -0,0 +1,14 @@
+package com.dermai.common.constants;
+
+public class RedisConstants {
+
+    /**
+     * Captcha redis key
+     */
+    public static final String CAPTCHA_CODE_KEY = "captcha_codes:";
+
+    /**
+     * Captcha validity period (minutes)
+     */
+    public static final Integer CAPTCHA_EXPIRATION = 2;
+}
