eb28e19b19fefd7fb012bee2d75b2d5b0bf0a17c
shaobo <shaobolyu2001@gmail.com>
2024-03-31 01:18:33 +0800

    Authentication Failure Handler

---
+++ src/main/java/com/dermai/framework/security/handler/AuthenticationEntryPointImpl.java
@@ -0,0 +1,30 @@
+package com.dermai.framework.security.handler;
+
+import com.alibaba.fastjson2.JSON;
+import com.dermai.common.Utils.ServletUtils;
+import com.dermai.common.constants.HttpStatus;
+import com.dermai.framework.web.domain.AjaxResult;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Authentication Failure Handler
+ *
+ * @author Shaobo
+ */
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint, Serializable {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        int code = HttpStatus.UNAUTHORIZED;
+        String msg = "Request for access: " + request.getRequestURI() +"failed, Unable to access system resources";
+        ServletUtils.renderString(response, JSON.toJSONString(AjaxResult.error(code, msg)));
+    }
+}
